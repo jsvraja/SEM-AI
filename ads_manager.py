2ad009462111d83afe703feb3777d0fe400e31cb
jsvking <sakthivel.raja@zohocorp.com>
2026-04-01 22:56:54 +0530

    Try v16 REST API

--- ads_manager.py
+++ ads_manager.py
@@ -9,7 +9,7 @@ import httpx
 from datetime import datetime, date
 from typing import Optional
 
-GOOGLE_ADS_BASE = "https://googleads.googleapis.com/v19"
+GOOGLE_ADS_BASE = "https://googleads.googleapis.com/v16"
 
 
 def get_headers(refresh_token: str) -> dict:
