2b0d3b1374b0419a7e80cf6dd4f3083d1d74d646
jsvking <sakthivel.raja@zohocorp.com>
2026-04-02 14:34:51 +0530

    Fix Request import

--- main_with_ads.py
+++ main_with_ads.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
